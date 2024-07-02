eba010f4d4ce9e7bca3c3dc77c0ae81f22fb2185
dvomar <zzomarzz6@gmail.com>
2024-07-02 07:29:23 +0300

    .

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostsController extends Controller
+{
+    public function index(){
+        $posts = Post::with('user')->latest()->get();
+        return inertia::render('Posts/index',[
+            'posts'=>PostResource::collection($posts)
+        ]);
+    }
+    public function store(PostRequest $request)
+    {
+        auth()->user()->posts()->create($request->validated());
+        return redirect('posts')->with('notification',[
+            'type' => 'success',
+            'msg' => 'Post Created Successfully'
+        ]);
+    }
+
+}
